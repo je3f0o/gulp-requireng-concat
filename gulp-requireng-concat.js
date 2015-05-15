48a61e33aaca28e7369a5f09561da36b37766453
je3f0o <je3f0o@gmail.com>
2015-05-16 00:04:53 +0800

    fixed relative path

--- gulp-requireng-concat.js
+++ gulp-requireng-concat.js
@@ -2,14 +2,15 @@
 * File Name  : gulp-requireng-concat.js
 * Purpose    :
 * Created at : 2015-05-15
-* Updated at : 2015-05-15
+* Updated at : 2015-05-16
 * Author     : jeefo
 _._._._._._._._._._._._._._._._._._._._._.*/
 
 "use strict";
 
-var gutil     = require("gulp-util"),
-	Builder   = require("./lib/builder"),
+var path    = require("path"),
+	gutil   = require("gulp-util"),
+	Builder = require("./lib/builder"),
 
 	PluginError = gutil.PluginError;
 
@@ -17,6 +18,8 @@ module.exports = function(requireng_path) {
 	if (! requireng_path) {
 		throw new PluginError("gulp-requireng-concat", "Missing requireng.json file's path for gulp-requireng-concat");
 	}
+
+	requireng_path = path.join(process.cwd(), requireng_path);
 
 	var builder = new Builder(requireng_path);
 	
